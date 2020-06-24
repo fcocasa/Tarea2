7796b21c329d242fccbf237e9a838f9c6a82d23e
martin.borba <m.borba98@gmail.com>
2020-06-24 18:15:20 -0300

    modificando p0

--- Tarea2/src/programas/programa1.py
+++ Tarea2/src/programas/programa1.py
@@ -8,18 +8,24 @@ def tokenize(text):
     return list(text)
 
 def parse(s):
-    grammarString = """
-    S -> 'a' S 'b' | 'a' 'b' 
+   grammarString = """
+    S-> 'A' Q | 'B' Q | 'C' R | 'A' | 'B' 
+    P-> 'A' Q | 'B' Q | 'C' R | 'A' | 'B' 
+    Q-> 'C'U | 'A'R | 'B'R 
+    R-> 'A'T | 'B'T | 'C'P | 'A' | 'B' | 'C'
+    U-> 'A'P | 'B'P | 'C'Q | 'A' | 'B' | 'C'
+    T-> 'A'P | 'B'P | 'C'Q | 'A' | 'B' | 'C'
     """
     grammar = nltk.CFG.fromstring(grammarString)
     s_tok = tokenize(s.strip())
     parser = nltk.LeftCornerChartParser(grammar)
-    tree = [t for t in parser.parse(s_tok)][:1]
+    tree = [t for t in parser.parse(s_tok)][:1]  # [:1] toma el primer tree q encuentra
     return tree
 
 if __name__ == '__main__':
-    archivo_entrada ="../entradas/p1entrada1.txt" # sys.argv[1]
-    archivo_salida ="programa1-entrada1.txt" # sys.argv[2]
+    archivo_entrada = sys.argv[1] # "../entradas/p1entrada1.txt" 
+    archivo_salida = sys.argv[2] # "programa1-entrada1.txt" 
+
     f = io.open(archivo_entrada, 'r', newline='\n', encoding='utf-8')
     s = f.read()
     f.close()

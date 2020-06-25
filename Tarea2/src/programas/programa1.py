5de1aa7e436f53c39935adf524641a24577f9da1
fcocasa <44985620+fcocasa@users.noreply.github.com>
2020-06-24 22:13:47 -0300

    Programa 1 terminado

--- Tarea2/src/programas/programa1.py
+++ Tarea2/src/programas/programa1.py
@@ -6,26 +6,25 @@ import nltk
 
 def tokenize(text):
     return list(text)
-
+#'A' Q | 'B' Q | 
 def parse(s):
-   grammarString = """
-    S-> 'A' Q | 'B' Q | 'C' R | 'A' | 'B' 
-    P-> 'A' Q | 'B' Q | 'C' R | 'A' | 'B' 
-    Q-> 'C'U | 'A'R | 'B'R 
-    R-> 'A'T | 'B'T | 'C'P | 'A' | 'B' | 'C'
-    U-> 'A'P | 'B'P | 'C'Q | 'A' | 'B' | 'C'
-    T-> 'A'P | 'B'P | 'C'Q | 'A' | 'B' | 'C'
+    grammarString = """
+    S -> 'A' | 'B' | 'A' P | 'B' P | 'C' Q
+    P -> 'C' U | 'A' Q | 'B' Q
+    U -> 'A' | 'B' | 'C' | 'A' T | 'B' T | 'C' P
+    T -> 'A' | 'B' | 'C' Q | 'A' P | 'B' P
+    Q -> 'A' | 'B' | 'C' | 'A' R | 'B' R | 'C' T
+    R -> 'C' | 'A' Q | 'B' Q
     """
     grammar = nltk.CFG.fromstring(grammarString)
     s_tok = tokenize(s.strip())
     parser = nltk.LeftCornerChartParser(grammar)
-    tree = [t for t in parser.parse(s_tok)][:1]  # [:1] toma el primer tree q encuentra
+    tree = [t for t in parser.parse(s_tok)][:1]
     return tree
 
 if __name__ == '__main__':
-    archivo_entrada = sys.argv[1] # "../entradas/p1entrada1.txt" 
-    archivo_salida = sys.argv[2] # "programa1-entrada1.txt" 
-
+    archivo_entrada = sys.argv[1]
+    archivo_salida = sys.argv[2]
     f = io.open(archivo_entrada, 'r', newline='\n', encoding='utf-8')
     s = f.read()
     f.close()
